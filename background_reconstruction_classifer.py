8f797766b741968065e239ac93b13166c950b8ba
dingb <dingb@mcmaster.ca>
2022-06-21 21:20:55 -0400

    added resnet based classifer v1

--- background_reconstruction_classifer.py
+++ background_reconstruction_classifer.py
@@ -50,26 +50,40 @@ def erosion(gray, idx):
     return eroded
 
 
-def res_classifer(image, categories, model):
+def res_classifer(image, categories, model, batch=64):
 
-    input_image = Image.fromarray(image)
+    input_image = [Image.fromarray(ii) for ii in image]
     preprocess = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    input_tensor = preprocess(input_image)
-    input_batch = input_tensor.unsqueeze(0)  # create a mini-batch as expected by the model
+
+    # test_sample = preprocess(input_image[4])
+    # print(test_sample.shape)
+    #
+    # test_sample = test_sample.permute(1, 2, 0).numpy()
+    # cv2.imshow('0', test_sample)
+    # cv2.waitKey(0)
+
+    input_batch = torch.zeros((batch, 3, 224, 224))
+    for ii, jj in enumerate(input_image):
+        input_batch[ii] = preprocess(jj)
+    print(input_batch.shape)
+    # input_batch = input_tensor.unsqueeze(0)  # create a mini-batch as expected by the model
     input_batch = input_batch.to('cuda')
     model.to('cuda')
     t = time.time()
     output = model(input_batch)
     print(time.time()-t)
-    probabilities = torch.nn.functional.softmax(output[0], dim=0)
-    top5_prob, top5_catid = torch.topk(probabilities, 5)
+    out = []
+    for ii in range(batch):
+        probabilities = torch.nn.functional.softmax(output[ii], dim=0)
+        top5_prob, top5_catid = torch.topk(probabilities, 5)
+        out.append((categories[top5_catid[0]], top5_prob[0]))
 
-    return categories[top5_catid[0]], top5_prob[0]
+    return out
 
 
 # Opens the Video file
@@ -79,6 +93,7 @@ model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
 background = cv2.imread('background_test.jpg', cv2.IMREAD_GRAYSCALE)
 mot_tracker = Sort(max_age=30, min_hits=3, iou_threshold=0.05)
 detectionidx = 0
+
 with open("imagenet_classes.txt", "r") as f:
     categories = [s.strip() for s in f.readlines()]
 
@@ -87,38 +102,29 @@ ret, ppv_frame = cap.read()
 ppv_frame = cv2.cvtColor(ppv_frame, cv2.COLOR_BGR2GRAY)
 ret, prev_frame = cap.read()
 prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG(nmixtures=4, history=5000)
 while(cap.isOpened()):
+
     ret, frame = cap.read()
     if not ret:
         break
     display = frame.copy()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     background_sub = cv2.subtract(gray, background)
     _, background_sub = cv2.threshold(background_sub, 20, 255, cv2.THRESH_BINARY)
-
-    flow_sub = cv2.subtract(gray, prev_frame)
-    # flow_sub = cv2.cvtColor(flow_sub, cv2.COLOR_BGR2GRAY)
-    _, flow_sub = cv2.threshold(flow_sub, 50, 255, cv2.THRESH_BINARY)
-
-    flow_sub_2 = cv2.subtract(gray, ppv_frame)
-    # # flow_sub_2 = cv2.cvtColor(flow_sub_2, cv2.COLOR_BGR2GRAY)
-    _, flow_sub_2 = cv2.threshold(flow_sub_2, 50, 255, cv2.THRESH_BINARY)
-
-    mask = cv2.addWeighted(flow_sub, 0.5, background_sub, 0.5, 0)
+    fgmask = fgbg.apply(frame)
+    mask = cv2.addWeighted(fgmask, 0.5, background_sub, 0.5, 0)
     # mask = cv2.addWeighted(mask, 0.5, flow_sub_2, 0.5, 0)
 
-    median = cv2.medianBlur(mask, 5)
+    median = cv2.medianBlur(mask, 7)
     ret, thresh = cv2.threshold(median, 1, 255, 0)
-    thresh = dilatation(thresh, 12)
+    thresh = dilatation(thresh, 3)
     # thresh = erosion(thresh, 2)
     # masked = cv2.bitwise_and(frame, frame, mask=median)
-    # opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    #
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # contours = cv2.convexHull(contours)
     dets = []
-
+    detections=[]
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
 
@@ -126,14 +132,17 @@ while(cap.isOpened()):
         a = w*h
         if a > 200:
             dets.append([x1, y1, x2, y2, 0 ,0])
-            detection = frame[y:y + h, x:x + w, :]
-            cls, conf = res_classifer(detection, categories, model)
-            # print(c)
-            cv2.rectangle(display, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.putText(display, f"{cls}, {conf:2f} ", (x, y+10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        [255, 255, 0], 2)
+            detections.append(frame[y:y + h, x:x + w, :])
+
             detectionidx+=1
+    output = res_classifer(detections, categories, model)
+    for ii, jj in enumerate(dets):
+        cv2.rectangle(display, (jj[0], jj[1]), (jj[2], jj[3]), (0, 0, 255), 2)
+        cv2.putText(display, f"{output[ii][0]}, {output[ii][1]:2f} ", (jj[0], jj[1] + 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    [255, 255, 0], 2)
+    # print(c)
+    # cv2.rectangle(display, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     if dets == []:
         dets = np.empty((0, 6))
